67a2086100f420df52e87671f6d9e5842ef9fbf0
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-07 12:16:44 +0200

    fix

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 
-// Подключение к базе данных
-
-
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -17,10 +14,9 @@ app.use(express.json());
 // Маршруты
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
-
 connectDB()
     .then(()=> {
+        const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     });
 
